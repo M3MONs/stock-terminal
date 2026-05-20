8abf02b7480423f853f87b317382cc3b43215549
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-20 19:30:04 +0200

    feat: add AppConfig model for application configuration

---
+++ models/app_config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+from models.timeframe import Timeframe
+
+
+class AppConfig(BaseModel):
+    provider: str = ""
+    default_timeframe: Timeframe = Timeframe.H1
+    refresh_interval: int = 300
+    theme: str = "dark"
