266b46455a14ba7b79b8293f5f6cde5f4c90397e
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-21 18:45:08 +0200

    feat: implement SymbolService for managing symbols and their validation

---
+++ services/symbol_service.py
@@ -0,0 +1,24 @@
+from models.tagged_symbol import TaggedSymbol
+from validators.validators import DefaultSymbolValidator, SymbolValidator
+from repositories.symbol_repository import SymbolRepository
+
+
+class SymbolService:
+    def __init__(self, repository: SymbolRepository, validator: SymbolValidator | None = None) -> None:
+        self._repository = repository
+        self._validator = validator or DefaultSymbolValidator()
+
+    def get_all(self) -> list[TaggedSymbol]:
+        return self._repository.get_all()
+
+    def add(self, symbol: str) -> None:
+        error = self._validator.validate(symbol)
+        if error:
+            raise ValueError(error)
+        self._repository.add(symbol)
+
+    def remove(self, symbol: str) -> None:
+        self._repository.remove(symbol)
+
+    def update_tags(self, symbol: str, tags: list[str]) -> None:
+        self._repository.update_tags(symbol, tags)
