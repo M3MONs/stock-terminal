8ebf222bcca1c7aef383dea6d6856b8adcf9f0d8
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-23 12:57:10 +0200

    feat: update ConfirmModal styles for alignment and structure

--- ui/components/confirm_modal/styles.py
+++ ui/components/confirm_modal/styles.py
@@ -1,11 +1,13 @@
 CSS = """
+ConfirmModal {
+    align: center middle;
+}
 ConfirmModal > Vertical#confirm-dialog {
     width: 50;
     height: auto;
     background: $surface;
     border: solid $accent;
     padding: 1 2;
-    align: center middle;
 }
 ConfirmModal Label { margin-bottom: 1; }
 ConfirmModal Horizontal { align: center middle; height: auto; }
