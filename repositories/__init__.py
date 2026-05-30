2d1179386bc8d119741b890c5b47e2240bd6deb2
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-30 09:50:36 +0200

    feat: initialize UserAgentRepository in the repository module

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,5 +1,7 @@
 from db import get_connection
 from .symbol_repository import SymbolRepository
+from .user_agent_repository import UserAgentRepository
 
 
 symbol_repo = SymbolRepository(connection_factory=get_connection)
+user_agent_repo = UserAgentRepository(connection_factory=get_connection)
