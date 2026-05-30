cd26a3c6a725e88ce2bc34f965b8e95894625284
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-05-30 17:26:15 +0200

    feat: initialize UserAgentRecommendationRepository for user agent recommendations

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,7 +1,9 @@
 from db import get_connection
 from .symbol_repository import SymbolRepository
+from .user_agent_recommendation_repository import UserAgentRecommendationRepository
 from .user_agent_repository import UserAgentRepository
 
 
 symbol_repo = SymbolRepository(connection_factory=get_connection)
 user_agent_repo = UserAgentRepository(connection_factory=get_connection)
+recommendation_repo = UserAgentRecommendationRepository(connection_factory=get_connection)
