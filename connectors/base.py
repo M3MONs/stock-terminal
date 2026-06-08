a2b5e0578510a67eba38c8b441bd49b3f182082b
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-06-08 19:06:20 +0200

    refactor: remove unused config attribute from BaseAgentConnector

--- connectors/base.py
+++ connectors/base.py
@@ -12,7 +12,6 @@ class BaseAgentConnector(ABC):
     def __init__(self, model_name: str, temperature: float = 0.0, **kwargs) -> None:
         self.model_name = model_name
         self.temperature = temperature
-        self.config = kwargs
 
     @abstractmethod
     def generate_structured(self, prompt: str, response_model: type[T], **kwargs) -> T: ...
